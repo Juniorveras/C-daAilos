2900538d1cafd51a09f1675803ef273b3a62abc3
Junior Veras <45885190+Juniorveras@users.noreply.github.com>
2024-09-14 21:16:33 -0300

    Questão 1
    Uma instituição financeira solicita que para cadastrar uma conta bancária, é necessário informar:
    •	o número da conta, 
    •	o nome do titular da conta, 
    •	e o valor de depósito inicial que o titular depositou ao abrir a conta.
    
    O valor de depósito inicial, é opcional, ou seja: 
    •	se o titular não tiver dinheiro a depositar no momento de abrir sua conta, o depósito inicial não será feito e o saldo inicial da conta será, naturalmente, zero.

---
+++ ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao_1
+{
+    internal class ContaBancaria
+    {
+        private int _numeroConta;
+        private string _nomeTitular;
+        private double _saldo;
+        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial = 0)
+        {
+            _numeroConta = numeroConta;
+            _nomeTitular = nomeTitular;
+            _saldo = depositoInicial;
+        }
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+        public void Sacar(double valor)
+        {
+            //if (_saldo - valor - 3.50 <= 0)
+            //{
+            //    Console.WriteLine("Saldo insuficiente para realizar o saque.");
+            //}
+            //else
+            //{
+                Console.WriteLine("Saldo insuficiente para realizar o saque. Consumira do cheque-especial saldo ira se tornar negativo");
+                _saldo -= valor + 3.50;
+            //}
+        }
+        public void AlterarTitular(string novoNomeTitular)
+        {
+            _nomeTitular = novoNomeTitular;
+        }
+        public override string ToString()
+        {
+            return $"Conta {_numeroConta}, Titular: {_nomeTitular}, Saldo: ${_saldo:F2}";
+        }
+    }
+}
